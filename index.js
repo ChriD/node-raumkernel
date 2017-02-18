e99e1d7cdb054784ee4367b376cf9b84e0b3b749
Christian Dürnberger <chris_d85@hotmail.com>
2017-02-18 19:09:26 +0100

    0.0.2

--- index.js
+++ index.js
@@ -12,11 +12,44 @@ var raumkernel = new Raumkernel();
 raumkernel.createLogger(5);
 raumkernel.init();
 
+/*
 raumkernel.managerDisposer.deviceManager.on("mediaRendererRaumfeldVirtualAdded", function (_usn, _device){
         raumkernel.logWarning("Event Emitted for device: " + _usn);
+        _device.changeVolume(-10).then(function (_volume){
+            raumkernel.logWarning("###### Done");
+            }).catch(function ()
+            {
+            raumkernel.logError("###### Get Info error");
+            }
+            
+            );
+    });
+    
+    
+    raumkernel.managerDisposer.deviceManager.on("mediaRendererAdded", function (_usn, _device){
+        raumkernel.logWarning("Event Emitted for device: " + _usn);
+        
+        _device.setVolume(80).then(function (_volume){
+            raumkernel.logWarning("###### SETVolume: " + JSON.stringify(_volume));
+            }).catch(function ()
+            {
+            raumkernel.logError("###### Set Volume error");
+            }
+            
+            );
+        
+        
+        _device.getVolume().then(function (_volume){
+            raumkernel.logWarning("###### Volume: " + _volume);
+            }).catch(function ()
+            {
+            raumkernel.logError("###### Get Volume error");
+            }
+            
+            );
     });
 
-
+*/
 function execute(){
    //code to execute
    //client.search('urn:schemas-upnp-org:device:MediaRenderer:1');
