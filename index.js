e998b4981371c9f4f502edc9fe0e36eece54c1c1
Christian Dürnberger <chris_d85@hotmail.com>
2017-02-23 17:38:27 +0100

    1.0.6

--- index.js
+++ index.js
@@ -2,4 +2,18 @@
 
 var path = './lib/';
 
-module.exports = require(path + "lib.raumkernel");
+module.exports = {   
+    Raumkernel : require(path + "lib.raumkernel"),
+    Logger : require(path + "lib.logger"),
+    Base : require(path + "lib.base"),
+    BaseManager : require(path + "lib.base.manager"),
+    MediaRenderer : require(path + "lib.device.upnp.mediaRenderer"),
+    MediaRendererRaumfeld : require(path + "lib.device.upnp.mediaRenderer.raumfeld"),
+    MediaRendererRaumfeldVirtual : require(path + "lib.device.upnp.mediaRenderer.raumfeldVirtual"),
+    MediaServer : require(path + "lib.device.upnp.mediaServer"),
+    MediaServerRaumfeld : require(path + "lib.device.upnp.mediaServer.raumfeld"),
+    ManagerBase : require(path + "lib.manager.base"),    
+    DeviceManager : require(path + "lib.manager.deviceManager"),
+    ZoneManager : require(path + "lib.manager.zoneManager"),
+    ManagerDisposer : require(path + "lib.managerDisposer"),
+}
