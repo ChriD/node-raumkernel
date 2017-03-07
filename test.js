21e99ccd338d41a7afa7f0fdc078aac1991e0802
Christian Dürnberger <chris_d85@hotmail.com>
2017-03-07 19:33:23 +0100

    1.0.10

--- test.js
+++ test.js
@@ -3,8 +3,30 @@ var Raumkernel = require('./lib/lib.raumkernel');
 
 var raumkernel = new Raumkernel();
 
-raumkernel.createLogger(3);
+raumkernel.createLogger(2);
 raumkernel.init();
+
+
+raumkernel.on("mediaServerRaumfeldAdded", function(_udn, _mediaServer){
+        
+        raumkernel.logWarning("Raumfeld Media Server found!");
+        /*
+        _mediaServer.browse("0").then(function(_data){
+                raumkernel.logWarning(JSON.stringify(_data));
+            })
+            
+        _mediaServer.browse("0/My Music").then(function(_data){
+                raumkernel.logWarning(JSON.stringify(_data));
+            })
+        */
+        
+        raumkernel.managerDisposer.mediaListManager.getMediaList(0).then(function(_data){
+                raumkernel.logWarning(JSON.stringify(_data));
+            }).catch(function(_data){
+                raumkernel.logError(JSON.stringify(_data));
+            });
+
+    });
 
 
 function execute(){
