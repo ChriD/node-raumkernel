5958934399491fdd0c0a42f827ac1b6aa81263e3
Christian Dürnberger <chris_d85@hotmail.com>
2017-03-10 19:21:14 +0100

    1.0.10 - #10 #11

--- test.js
+++ test.js
@@ -7,9 +7,22 @@ raumkernel.createLogger(2);
 raumkernel.init();
 
 
+raumkernel.on("systemReady", function(_ready){
+        raumkernel.logInfo("System ready: " + _ready);
+    });
+    
+raumkernel.on("mediaListReady", function(_listId, _data){
+        raumkernel.logInfo("MediaList ready: " + _listId);
+    });
+    
+    
+raumkernel.on("mediaRendererPlaylistReady", function(_listId, _data){
+        raumkernel.logInfo("mediaRendererPlaylistReady ready: " + _listId);
+    });
+
 raumkernel.on("mediaServerRaumfeldAdded", function(_udn, _mediaServer){
         
-        raumkernel.logWarning("Raumfeld Media Server found!");
+        //raumkernel.logWarning("Raumfeld Media Server found!");
         /*
         _mediaServer.browse("0").then(function(_data){
                 raumkernel.logWarning(JSON.stringify(_data));
@@ -20,11 +33,13 @@ raumkernel.on("mediaServerRaumfeldAdded", function(_udn, _mediaServer){
             })
         */
         
-        raumkernel.managerDisposer.mediaListManager.getMediaList(0).then(function(_data){
+        /*
+        raumkernel.managerDisposer.mediaListManager.getMediaList("0", 0).then(function(_data){
                 raumkernel.logWarning(JSON.stringify(_data));
             }).catch(function(_data){
                 raumkernel.logError(JSON.stringify(_data));
             });
+            */
 
     });
 
